17161d424e4448b229e7186bc84104f42db11674
HoHyunKim-kr <hohyun1022@naver.com>
2025-02-25 16:17:21 +0900

    feat : study zombies

---
+++ Chap1/chap1-3.sol
@@ -0,0 +1,7 @@
+//
+pragma solidity ^0.8.17; 
+
+// ㅅ
+contract ZombieFactory{
+    uint dnaDigits = 16;
+}
---
+++ Chap1/chap1-4.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.17; 
+
+contract ZombieFactory {
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+}
